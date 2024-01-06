7c320dddbc60eb409f5d329ebdcb3b2bff7592ff
Basharul Alam Mazu <112147775+Basharul2002@users.noreply.github.com>
2024-01-06 18:13:48 +0600

    Update 1. Reverse Array.cpp

--- 1D Array/1. Reverse Array.cpp
+++ 1D Array/1. Reverse Array.cpp
@@ -8,8 +8,31 @@ Array_2 = {1,2,3,4,5,6,7,8}
 Output: 8 7 6 5 4 3 2 1 50 40 30 20 10
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
 void arrayDisplay(int a[], int n);
 void reverse(int arr[], int l);
+void arrayMarge(int arr1[], int arr2[], int s1, int s2);
+
+int marge[100];
+
+int main()
+{
+    int arr1[] = {10, 20, 30, 40, 50};
+    int arr2[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int s1=sizeof(arr1)/sizeof(arr1[0]);
+    int s2= sizeof(arr2)/sizeof(arr2[0]);
+
+    arrayDisplay(arr1, s1);
+    arrayDisplay(arr2, s2);
+    reverse(arr1, s1);
+    reverse(arr2, s2);
+
+    arrayMarge(arr1, arr2, s1, s2);
+    return 0;
+}
 
 
 void arrayDisplay(int a[], int n)
@@ -26,6 +49,7 @@ void reverse(int arr[], int l)
 {
     reverse(arr, arr+l);
 }
+
 
 void arrayMarge(int arr1[], int arr2[], int s1, int s2)
 {
