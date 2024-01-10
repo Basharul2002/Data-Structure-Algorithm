e8449635d83c51340aa569088827ba01bb91476c
Basharul Alam Mazu <112147775+Basharul2002@users.noreply.github.com>
2024-01-10 22:23:33 +0600

    Create Knapsack.cpp

---
+++ Greedy Algorithm/Knapsack.cpp
@@ -0,0 +1,86 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool compare(Item a, Item b)
+{
+    return ((double)a.value / a.weight) > ((double)b.value / b.weight);
+}
+
+double fractionalKnapsack(Item items[], int n, int capacity)
+{
+    sort(items, items + n, compare);
+
+    double totalValue = 0.0;
+    int currentWeight = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (currentWeight + items[i].weight <= capacity)
+        {
+            currentWeight += items[i].weight;
+            totalValue += items[i].value;
+        }
+
+        else
+        {
+            int remainingCapacity = capacity - currentWeight;
+            totalValue += (double)remainingCapacity * items[i].value / items[i].weight;
+            break;
+        }
+    }
+
+    return totalValue;
+}
+
+int knapsack01(Item items[], int n, int capacity)
+{
+    int bag[n+1][capacity+1];
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= capacity; w++)
+        {
+            if (i == 0 || w == 0)
+                bag[i][w] = 0;
+
+            else if (items[i - 1].weight <= w)
+                bag[i][w] = max(items[i - 1].value + bag[i - 1][w - items[i - 1].weight], bag[i - 1][w]);
+
+            else
+                bag[i][w] = bag[i - 1][w];
+        }
+    }
+
+    return bag[n][capacity];
+}
+
+int main()
+{
+    int n, capacity;
+    cout<<"Enter the number of items: ";
+    cin>>n;
+    cout<<endl;
+    Item items[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"Item-"<<i+1<<endl<<"******"<<endl;
+        cout<<"Enter the weight: ";
+        cin>>items[i].weight;
+        cout<<"Enter the profit: ";
+        cin>>items[i].value;
+
+        cout<<endl;
+    }
+
+    cout<<endl<<"Enter the knapsack capacity: ";
+    cin>>capacity;
+
+    cout<<endl<<endl<<"Optimal Solution"<<endl<<"****************"<<endl;
+    cout<<"Maximum profit using Fractional Knapsack: "<<(double)fractionalKnapsack(items, n, capacity)<<endl;
+    cout<<"Maximum profit using 0/1 Knapsack: "<<knapsack01(items, n, capacity);
+
+
+    cout<<endl<<endl;
+    return 0;
+}
