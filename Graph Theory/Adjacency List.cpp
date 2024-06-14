2a839cc4cda7cc63cdb670011380b782afb318bc
Basharul Alam Mazu <112147775+Basharul2002@users.noreply.github.com>
2024-06-14 16:26:05 +0600

    Add Adjacency Matrix

--- Graph Theory/Adjacency List.cpp
+++ Graph Theory/Adjacency List.cpp
@@ -3,34 +3,51 @@ using namespace std;
 
 int main()
 {
-    int nodes, edges;
+    int nodes, edges, x, y;
+    vector<int> graph[100000];
     
-    cout << "Enter the number of nodes: ";
+    cout << "Number of nodes: ";
     cin >> nodes;
     
-    cout << "Enter the number of edges: ";
+    cout << "Number of edges: ";
     cin >> edges;
     
-    // Initialize the adjacency list
-    vector<int> graph[100];
-    
-    int x, y;
-    cout << "Enter the edges (node pairs):" << endl;
-    for(int i = 0; i < edges; ++i)
+    cout << "Enter the node pairs:\n";
+    for (int i = 0; i < edges; ++i)
     {
         cin >> x >> y;
         graph[x].push_back(y);
         graph[y].push_back(x);
     }
     
+    // Print the adjacency matrix
+     cout << "Adjacency Matrix:\n";
+    for (int i = 0; i < nodes; ++i)
+    {
+        for (int j = 0; j < nodes; ++j)
+        {
+            bool edgeExists = false;
+            for (int k = 0; k < graph[i].size(); ++k)
+            {
+                if (graph[i][k] == j)
+                {
+                    edgeExists = true;
+                    break;
+                }
+            }
+            cout << (edgeExists ? "1 " : "0 ");
+        }
+        cout << "\n";
+    }
+    
     // Print the adjacency list
-    for(int i = 0; i < nodes; ++i)
+    cout << "\n\nAdjacency List:\n";
+    for (int i = 0; i < nodes; i++)
     {
         cout << i;
-        for(int j = 0; j < graph[i].size(); j++)
-            cout << "->" << graph[i][j];
-        
-        cout << endl;
+        for (int j = 0; j < graph[i].size(); j++)
+            cout << " -> " << graph[i][j];
+        cout << "\n";
     }
     
     return 0;
